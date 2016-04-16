08b980ad0ad09a44c5c9fa3abeb510279d6f0893
Kevin Glick <kevinglick@Kevins-MacBook-Air.local>
2016-04-16 18:31:24 -0400

    add switch statement

--- text-adventure-game/Assets/TextController.cs
+++ text-adventure-game/Assets/TextController.cs
@@ -8,7 +8,6 @@ public class TextController : MonoBehaviour
 	private enum States {cell, cell_2, mirror, cell_mirror, sheets_0, locks_0, sheets_1, locks_1, corridor_0, corridor_1,
 		corridor_2, corridor_3, stairs_0, stairs_1, stairs_2, floor, closet_door, in_closet, courtyard, freedom};
 	private States myState;
-	private bool hasHairClip = false;
 
  	// Use this for initialization
 	void Start ()
@@ -20,51 +19,72 @@ public class TextController : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		if(myState == States.cell) {
-			state_cell();
-		} else if(myState == States.sheets_0) {
-			state_sheets_0();
-		} else if(myState == States.locks_0) {
-			state_locks_0();
-		} else if(myState == States.mirror) {
-			state_mirror();
-		} else if(myState == States.cell_mirror) {
-			state_cell_mirror();
-		} else if(myState == States.sheets_1) {
-			state_sheets_1();
-		} else if(myState == States.locks_1) {
-			state_locks_1();
-		} else if(myState == States.corridor_0) {
-			state_corridor_0();
-		} else if(myState == States.closet_door) {
-			state_closet_door();
-		} else if(myState == States.floor) {
-			state_floor ();
-		} else if(myState == States.stairs_0) {
-			state_stairs_0();
-		} else if(myState == States.corridor_1) {
-			state_corridor_1 ();
-		} else if(myState == States.in_closet) {
-			state_in_closet();
-		} else if(myState == States.stairs_1) {
-			state_stairs_1();
-		} else if(myState == States.corridor_2) {
-			state_corridor_2();
-		} else if(myState == States.stairs_2) {
-			state_stairs_2();
-		} else if(myState == States.corridor_3) {
-			state_corridor_3();
-		} else if(myState == States.courtyard) {
-			state_courtyard();
-		} else if(myState == States.cell_2) {
-			state_cell_2();
-		} else if(myState == States.freedom) {
-			state_freedom();
+		switch(myState) {
+			case States.cell:
+				cell ();
+				break;
+			case States.sheets_0:
+				sheets_0 ();
+				break;
+			case States.locks_0:
+				locks_0 ();
+				break;
+			case States.mirror:
+				mirror ();
+				break;
+			case States.cell_mirror:
+				cell_mirror ();
+				break;
+			case States.sheets_1:
+				sheets_1 ();
+				break;
+			case States.locks_1:
+				locks_1();
+				break;
+			case States.corridor_0:
+				corridor_0 ();
+				break;
+			case States.stairs_0:
+				stairs_0 ();
+				break;
+			case States.closet_door:
+				closet_door();
+				break;
+			case States.floor:
+				floor ();
+				break;
+			case States.in_closet:
+				in_closet ();
+				break;
+			case States.corridor_1:
+				corridor_1 ();
+				break;
+			case States.stairs_1:
+				stairs_1 ();
+				break;
+			case States.corridor_2:
+				corridor_2 ();
+				break;
+			case States.corridor_3:
+				corridor_3 ();
+				break;
+			case States.stairs_2:
+				stairs_2 ();
+				break;
+			case States.courtyard:
+				courtyard ();
+				break;
+			case States.cell_2:
+				cell_2 ();
+				break;
+			case States.freedom:
+				freedom ();
+				break;
 		}
 	
 	}
 
-	void state_cell() {
+	void cell() {
 		text.text = "You are in a prison cell, and you want to escape. There are " + 
 					"some dirty sheets on the bed, a mirror on the wall, and the door " +
 					"is locked from the outside.\n\n" +
@@ -79,7 +99,7 @@ public class TextController : MonoBehaviour
 		}
 	}
 
-	void state_sheets_0() {
+	void sheets_0() {
 		text.text = "You can't believe that you sleep in these things. Surely it's " + 
 					"time somebody changed them. The pleasures of prison life " +
 					"I guess!\n\n" +
@@ -90,7 +110,7 @@ public class TextController : MonoBehaviour
 		}
 	}
 
-	void state_locks_0() {
+	void locks_0() {
 		text.text = "This is one of those button locks. You have no idea what the " +
 					"combination is. You wish you could somehow see where the dirty " +
 					"fingerprints were, maybe that would help.\n\n" +
@@ -101,7 +121,7 @@ public class TextController : MonoBehaviour
 		}
 	}
 
-	void state_mirror() {
+	void mirror() {
 		text.text = "The dirty old mirror on the wall seems loose.\n\n" +
 					"Press T to Take the mirror, or R to Return to cell.";
 		
@@ -112,7 +132,7 @@ public class TextController : MonoBehaviour
 		}
 	}
 
-	void state_cell_mirror() {
+	void cell_mirror() {
 		text.text = "You are still in your cell and you STILL want to escape! There are " +
 					"some dirty sheets on the bed, a mark where the mirror was, " +
 					"and that pesky door is still there, and firmly locked!\n\n" +
@@ -125,7 +145,7 @@ public class TextController : MonoBehaviour
 		}
 	}
 
-	void state_sheets_1() {
+	void sheets_1() {
 		text.text = "Holding a mirror in your hand doesn't make the sheets look " +
 					"any better.\n\n" +
 					"Press R to Return to roaming your cell.";
@@ -135,7 +155,7 @@ public class TextController : MonoBehaviour
 		}
 	}
 
-	void state_locks_1() {
+	void locks_1() {
 		text.text = "You carefully put the mirror through the bars and turn it round " +
 					"so you can see the lock. You can just make out fingerprints around " +
 					"the buttons. You press the dirty buttons and hear a click.\n\n" +
@@ -148,7 +168,7 @@ public class TextController : MonoBehaviour
 		}
 	}
 
-	void state_corridor_0() {
+	void corridor_0() {
 		text.text = "The door swings open! It was awfully squeeky, but nobody seems to have heard anything. " +
 					"You're now standing in a long corridor. There is a flight of stairs going down, a " +
 					"closet door just a few feet away from you, and something small and shiny laying on the floor.\n\n" +
@@ -163,7 +183,7 @@ public class TextController : MonoBehaviour
 		}
 	}
 
-	void state_stairs_0() {
+	void stairs_0() {
 		text.text = "You start going down the stairs slowly. Everything seems to be going well, " +
 					"but then you suddenly hear something... Footsteps! Somebody is making his " +
 					"way up the staircase! If you don't act fast, you're bound to get caught!\n\n" +
@@ -174,7 +194,7 @@ public class TextController : MonoBehaviour
 		}
 	}
 
-	void state_closet_door() {
+	void closet_door() {
 
 		text.text = "You approach the closet door. It seems like it would be a good place to hide " +
 					"temporarily, while you think about what to do next. You grab the handle and try to " +
@@ -187,7 +207,7 @@ public class TextController : MonoBehaviour
 		}
 	}
 
-	void state_floor() {
+	void floor() {
 		text.text = "You kneel down to inspect the shiny object on the floor more closely and discover that it's " +
 					"just a hair pin. You aren't sure how it could possibly help you, but then again it couldn't hurt " +
 					"to take it with you just in case.\n\n" +
@@ -200,7 +220,7 @@ public class TextController : MonoBehaviour
 		}
 	}
 
-	void state_corridor_1() {
+	void corridor_1() {
 		text.text = "You get back to your feet and take a look around the room. There is still only one way out. " +
 					"You'll have to take the stairs. Although, you could try to hide in the closet and plan the rest " +
 					"of your escape without worrying about being seen.\n\n" +
@@ -213,7 +233,7 @@ public class TextController : MonoBehaviour
 		}
 	}
 
-	void state_stairs_1() {
+	void stairs_1() {
 		text.text = "You start heading down the stairs, but as you go you can hear footsteps and voices ahead. It's just " +
 					"too risky to continue on. You'll almost certainly get caught by someone. You need to take some time and " +
 					"plan what to do next.\n\n" +
@@ -224,8 +244,8 @@ public class TextController : MonoBehaviour
 		}
 	}
 
-	void state_in_closet() {
-		text.text = "You try the door hanlde, but it's firmly locked. There are voices on the stairs and they're growing louder." +
+	void in_closet() {
+		text.text = "You try the door handle, but it's firmly locked. There are voices on the stairs and they're growing louder." +
 					"You know you're about to get caught... But then you remember the hair clip, retrieve it from your pocket and manage " +
 					"to pick the lock and get into the closet. Just in the knick of time. While you wait for the guard to leave, you notice some " +
 					"fresh clothes folded and placed on a shelf. These aren't just any clothes, though. It's a guard's uniform! This could really be helpful!\n\n" +
@@ -238,7 +258,7 @@ public class TextController : MonoBehaviour
 		}
 	}
 
-	void state_corridor_2() {
+	void corridor_2() {
 		text.text = "You sneak back into the corridor and take a look around. It seems like the guard has gone. You can duck back " +
 					"into the closet and hide or you can try and make a break for it.\n\n" +
 					"Press C to get back in the Closet or S to make a mad dash up the Stairs";
@@ -250,7 +270,7 @@ public class TextController : MonoBehaviour
 		}
 	}
 
-	void state_stairs_2() {
+	void stairs_2() {
 		text.text = "As it turns out, that was a terrible decision. Just as you take your first steps up the stairs the sound of voices " +
 					"and footfalls returns. The guard is coming back! You're going to have to hide! And FAST!!\n\n" +
 					"Press H to run to the corridor and Hide in the closet";
@@ -260,7 +280,7 @@ public class TextController : MonoBehaviour
 		}
 	}
 
-	void state_corridor_3() {
+	void corridor_3() {
 		text.text = "You put on the guard's uniform and step out of the closet and into the corridor. A passing guard looks at you " +
 					"strangely and asks 'sleeping on the job, are we?'. You smile and nod as sweat drips from your brow. You're nervous, " +
 					"but you'll have to make a decision.\n\n" +
@@ -273,7 +293,7 @@ public class TextController : MonoBehaviour
 		}
 	}
 
-	void state_courtyard() {
+	void courtyard() {
 		text.text = "Almost home free! You feel the fresh air on your face as you exit the building and step into the courtyard. " +
 					"You can see the gate and it's open! However, it's surrounded by a guard on either side of it and sentries in " +
 					"the guard towers with scoped rifles. You feel a lump form in your throat and your stomach is tied up in knots.\n\n" +
@@ -286,7 +306,7 @@ public class TextController : MonoBehaviour
 		}
 	}
 
-	void state_cell_2() {
+	void cell_2() {
 		text.text = "Well, you're back in your cell. It's not paradise, but as you lay down on your dirty cot and calm your nerves down " +
 					"you decide you've made the right choice. You probably wouldn't have gotten away with it anyway and at least now you " +
 					"are free to pay your debts to society... for the next 50 years! 'Should have paid those parking tickets' you sigh as you " +
@@ -294,7 +314,7 @@ public class TextController : MonoBehaviour
 					"GAME OVER!!!";
 	}
 
-	void state_freedom() {
+	void freedom() {
 		text.text = "You smile nervously as you pass the guards. They wave to you in acknowledgement as you pass through the gates. " +
 					"You can hardly believe it as you walk right into the parking lot and hop on a bus. You know exactly where you're going to " +
 					"go. Straight to Mexico!! You are finally FREE!\n\n" +
