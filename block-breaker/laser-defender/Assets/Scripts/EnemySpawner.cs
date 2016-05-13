673a17e777c1215049616928c042770c70ade176
Kevin <kevin.n.glick@gmail.com>
2016-05-12 23:56:39 -0400

    add player and enemy sprites

---
+++ block-breaker/laser-defender/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+	public GameObject enemyPrefab;
+	public float speed = 5.5f;
+	public float width = 10.0f;
+	public float height = 5.0f;
+	private float minX;
+	private float maxX;
+	private bool moveRight;
+
+	// Use this for initialization
+	void Start () {
+		moveRight = true;
+		foreach(Transform child in transform) {
+			GameObject enemy = (GameObject)Instantiate(enemyPrefab, child.transform.position, Quaternion.identity);
+			enemy.transform.parent = child;
+
+			float distance = transform.position.z - Camera.main.transform.position.z;
+			Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+			Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+			minX = leftMost.x;
+			maxX = rightMost.x;
+		}
+	}
+
+	public void OnDrawGizmos() {
+		Gizmos.DrawWireCube (transform.position, new Vector3(width, height));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float rightEdgeOfFormation = transform.position.x + (width/2);
+		float leftEdgeOfFormation = transform.position.x - (width/2);
+
+		if(moveRight) {
+			transform.position += Vector3.right * speed * Time.deltaTime;
+			if(rightEdgeOfFormation >= maxX) {
+				moveRight = false;
+			}
+		} else {
+			transform.position += Vector3.left * speed * Time.deltaTime;
+			if(leftEdgeOfFormation <= minX) {
+					moveRight = true;
+				}
+		}
+
+	}
+}
---
+++ block-breaker/laser-defender/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 5.0f;
+	private float minX;
+	private float maxX;
+	public float padding = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+		Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+		minX = leftMost.x + padding;
+		maxX = rightMost.x - padding;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftArrow)) {
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		} else if(Input.GetKey(KeyCode.RightArrow)) {
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+
+		float newX = Mathf.Clamp (transform.position.x, minX, maxX);
+
+		transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+	}
+}
